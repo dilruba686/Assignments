7908464d82a5d25667d6baf6bf891ba11452ea12
dilruba686 <71407801+dilruba686@users.noreply.github.com>
2020-10-09 18:10:56 +0200

    Add files via upload

---
+++ Q11_ReversePyramid.java
@@ -0,0 +1,58 @@
+package assignments.assigment05_day12_forLoop;
+
+public class Q11_ReversePyramid {
+
+	public static void main(String[] args) {
+		
+	
+      
+		
+
+
+		int rows=6;
+		
+		
+		for(int i=1;i<=rows;i++) {
+                
+			
+			for(int a=2;a<=i;a++) {
+				
+				System.out.print("  ");
+				
+			}
+			for(int j=rows+1;j>i;j--) {
+				
+				
+				System.out.print(j-i+"   ");
+			
+			
+			    }
+			System.out.println();
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+	}
+
+
+
