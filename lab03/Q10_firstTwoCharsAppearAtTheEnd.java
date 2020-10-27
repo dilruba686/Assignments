114d2de0a7f68997a585d8fbbdb277efb40a65dc
dilruba686 <71407801+dilruba686@users.noreply.github.com>
2020-10-27 02:12:53 +0100

    Add files via upload

---
+++ lab03/Q10_firstTwoCharsAppearAtTheEnd.java
@@ -0,0 +1,34 @@
+package assignments.lab03;
+
+public class Q10_firstTwoCharsAppearAtTheEnd {
+
+	public static void main(String[] args) {
+		
+		
+//		Given a string, return true if the first 2 chars in the string also appear at
+//	    the end of the string, such as with "edited".
+//				frontAgain("edited") → true
+//				frontAgain("edit") → false
+//				frontAgain("ed") → true
+		
+
+		System.out.println(frontAgain("edited"));
+		
+	}
+
+	public static boolean frontAgain(String word) {
+		
+
+		String startsWith=word.substring(0, 2);
+		return word.endsWith(startsWith);
+		
+//  ---------Another option------
+		
+//		String endsWith=word.substring(word.length()-2);
+//		
+//		return word.startsWith(endsWith);
+		
+	}
+	
+	
+}
