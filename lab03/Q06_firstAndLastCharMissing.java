b8ed90e5d51dabc55be0b5f9fe1641f93947359c
dilruba686 <71407801+dilruba686@users.noreply.github.com>
2020-10-27 02:32:29 +0100

    Add files via upload

---
+++ lab03/Q06_firstAndLastCharMissing.java
@@ -0,0 +1,27 @@
+package assignments.lab03;
+
+public class Q06_firstAndLastCharMissing {
+
+	public static void main(String[] args) {
+		
+ 
+//		Given a string, return a version without the first and last char,
+//		so "Hello" yields "ell". The string length will be at least 2.
+//				withoutEnd("Hello") → "ell"
+//				withoutEnd("java") → "av"
+//				withoutEnd("coding") → "odin"
+		
+		System.out.println(withoutEnd("coding"));
+		
+		
+	}
+
+	public static String withoutEnd(String word) {
+		
+		return word.substring(1,word.length()-1);
+		
+		
+	}
+	
+	
+}
