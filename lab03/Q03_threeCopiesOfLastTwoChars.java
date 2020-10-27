9d4c994ac111f277ff3ffce8791d92f5213e619c
dilruba686 <71407801+dilruba686@users.noreply.github.com>
2020-10-27 02:34:55 +0100

    Add files via upload

---
+++ lab03/Q03_threeCopiesOfLastTwoChars.java
@@ -0,0 +1,28 @@
+package assignments.lab03;
+
+public class Q03_threeCopiesOfLastTwoChars {
+
+	public static void main(String[] args) {
+		
+//		    Create a method return a new string made of 3 copies of the last 2 chars of the original string. 
+//			The string length will be at least 2.
+//			extraEnd("Hello") → "lololo"
+//			extraEnd("ab") → "ababab"
+//			extraEnd("Hi") → "HiHiHi"
+		
+		System.out.println(extraEnd("Hi"));
+
+	}
+
+	
+	public static String extraEnd(String word) {
+		
+		if(word.length()<2) {
+			return "Invalid String";
+		}
+		String lastTwo=word.substring(word.length()-2);
+		word=lastTwo.repeat(3);
+		return word;
+		
+	}
+}
---
+++ lab03/Q09_rotateLastTwoCharsRight.java
@@ -0,0 +1,26 @@
+package assignments.lab03;
+
+public class Q09_rotateLastTwoCharsRight {
+
+	public static void main(String[] args) {
+	
+//		Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. 
+//	    The string length will be at least 2.
+//				right2("Hello") → "loHel"
+//				right2("java") → "vaja"
+//				right2("Hi") → "Hi"
+		
+		
+		System.out.println(right2("Hello"));
+		
+	}
+	public static String right2(String word) {
+		
+		if(word.length()<2) {
+			return "Invalid String";
+		}
+	return	word.substring(word.length()-2).concat(word.substring(0, word.length()-2));
+		
+	}
+
+}
